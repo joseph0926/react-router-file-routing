18c37f0e2cd544c809b9e98f29a825a958171678
joseph0926 <joseph0926@naver.com>
2024-10-09 11:55:23 +0900

    refactor: [2] 주석 추가

---
+++ src/utils/format-path.ts
@@ -0,0 +1,17 @@
+/**
+ * 파일/폴더 경로를 읽고 path로 변환하는 함수입니다.
+ * @param filePath 파일/폴더 경로
+ * @returns `path`: string
+ */
+export function formatPath(filePath: string): string {
+  let path = filePath
+    .replace(/^\/src\/pages/, "") // 폴더 경로에서 `path`에 포함되지 않을 부분 제거
+    .replace(/\/page\.(jsx|tsx)$/, "") // 폴더 경로에서 page.tsx or page.jsx 제거
+    .replace(/\[([^\]]+)\]/g, ":$1"); // [id] -> :id 로 변환
+
+  if (path === "") {
+    path = "/";
+  }
+
+  return path;
+}
