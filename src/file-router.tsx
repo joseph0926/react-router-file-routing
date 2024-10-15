5384cf9969691a01577d6cc6767d9009fe465204
김영훈 <joseph0926@naver.com>
2024-10-16 07:35:45 +0900

    fix: 스크립트 수정, 임포트 수정

--- src/file-router.tsx
+++ src/file-router.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useMemo } from 'react';
+import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { getRoutes } from './utils/get-routes';
 
@@ -14,13 +14,13 @@ export function FileRouter() {
   /**
    * 파일/폴더 정보를 읽은 후 해당 라우터 정보를 모두 가져옵니다
    */
-  const routes = useMemo(() => getRoutes(), []);
+  const routes = React.useMemo(() => getRoutes(), []);
 
-  const router = useMemo(() => createBrowserRouter(routes), [routes]);
+  const router = React.useMemo(() => createBrowserRouter(routes), [routes]);
 
   return (
-    <Suspense fallback={<div>Loading,,,</div>}>
+    <React.Suspense fallback={<div>Loading,,,</div>}>
       <RouterProvider router={router} />
-    </Suspense>
+    </React.Suspense>
   );
 }
